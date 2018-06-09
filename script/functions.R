14c6143756585d7ea3b05eca46f5f8debfa12b11
naoya1218 <hero12185@gmail.com>
2018-06-09 13:31:54 +0900

    ToS edit time
    ToS版を書いている途中

--- script/functions.R
+++ script/functions.R
@@ -20,8 +20,28 @@ garch_f_opt <- function(data, order = c(1, 1), error = "norm"){
       garch_loglik[i] <- log(dnorm(data[i], mean = 0, sd = sqrt(sigma_t[i])))
     }
     return(-sum(garch_loglik))
+    }
+    
+  garch_op_ToS <- function(params){
+    #sigma_tの計算
+    #計算できないところまでは、全体のボラティリティを与える
+    for(i in 1:p){
+      sigma_t[i] <- sum(data^2)/t
+      garch_loglik[i] <- log(dfas2(data[i], mean = 0, sd = sqrt(sigma_t[i])))
+    }
+    for(i in (p+1):t){
+      sigma_t[i] <- exp(params[1]) +
+        exp(params[2:(p+1)]) * sigma_t[(i-p):(i-1)] +
+        exp(params[(p+2):(p+q+1)]) * data[(i-q):(i-1)]^2
+      garch_loglik[i] <- log(dfas2(data[i], mean = 0, sd = sqrt(sigma_t[i])))
+    }
+    return(-sum(garch_loglik))
+    }
+  
+  if(grepl(error,"norm")){out <- optim(params, garch_op)}else{
+    if(grepl(error,"ToS")){out <- optim(params, garch_op_ToS)}
   }
-  optim(params, garch_op)
+  out
 }
 
 # garchモデルのsigmaを計算する関数
