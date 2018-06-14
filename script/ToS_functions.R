f4f95147cf47e43d7296da3af612cb2ea1e86b08
da-hie <hero12185@gmail.com>
2018-06-14 15:36:25 +0900

    GARCH ToS
    GARCHに対してToS分布を持ちいた結果追加
    ただし、分布の変換などの複雑なことはしておらず、
    パラメータsigmaの値をgarchで求めている

--- script/ToS_functions.R
+++ script/ToS_functions.R
@@ -26,3 +26,36 @@ dfas2 <- function(x, mu, sigma, lambda, delta){
   r <-s_inverse( (x-mu)/sigma, lambda)
   return(dfas(r, delta)/sigma)
 }
+
+# 局度変換を伴うsinh-arcsinh分布のパラメータ推定関数
+mle.dfas2 <- function(x, ini){
+  # 対数尤度を計算する関数
+  # (最適化の際にパラメータに制約があると望ましくないので指数変換している)
+  # (Rの最適化関数が最小化を目指すので正負を入れ替えてある) 
+  obj <- function(par){
+    mu <- par[1]
+    sigma <- exp(par[2])
+    lambda <- par[3]
+    delta <-par[4]
+    lik <-  -sum(log( dfas2(x, mu, sigma, lambda, delta)))
+    lik
+  }
+  # optimで上記の関数が最小になるパラメータiniを探してくれる
+  # BFGS法　興味のある人は自分で調べて
+  out <- optim( ini, obj, hessian=TRUE)
+  out$par2 <- out$par
+  out$par2[2] <- exp(out$par[2])
+  out
+}
+# 局度変換を伴うsinh-arcsinh(x)分布の平均，分散，歪度，尖度を計算する関数
+fas2.moment<- function(par){
+  f1 <- function(x) x*dfas2(x, mu=par[1], sigma=par[2], lambda=par[3], delta=par[4])
+  m1 <- integrate( f1, lower=-Inf, upper=Inf)$value
+  f2 <- function(x) (x-m1)^2*dfas2(x, mu=par[1], sigma=par[2], lambda=par[3], delta=par[4])
+  v2 <- integrate( f2, lower=-Inf, upper=Inf)$value
+  f3 <- function(x) (x-m1)^3*dfas2(x, mu=par[1], sigma=par[2], lambda=par[3], delta=par[4])/(v2^{3/2})
+  b1 <- integrate( f3, lower=-Inf, upper=Inf)$value
+  f4 <- function(x) (x-m1)^4*dfas2(x, mu=par[1], sigma=par[2], lambda=par[3], delta=par[4])/(v2^{2})
+  b2 <- integrate( f4, lower=-Inf, upper=Inf)$value
+  return(list(m1=m1, v2=v2,b1=b1, b2=b2)) 
+}
--- script/functions.R
+++ script/functions.R
@@ -1,4 +1,4 @@
-garch_f_opt <- function(data, order = c(1, 1), error = "norm"){
+garch_f_opt <- function(data, order = c(1, 1), error = "norm", dis_para = c()){
   t <- length(data)
   sigma_t <- rep(0, t)
   garch_loglik <- rep(0, t)
@@ -26,26 +26,34 @@ garch_f_opt <- function(data, order = c(1, 1), error = "norm"){
     #sigma_tの計算
     #計算できないところまでは、全体のボラティリティを与える
     for(i in 1:p){
-      sigma_t[i] <- sum(data^2)/t
-      garch_loglik[i] <- log(dfas2(data[i], mean = 0, sd = sqrt(sigma_t[i])))
+      #sigma_t[i] <- sum(data^2)/t
+      # ToS のsigma!=標準偏差なので
+      sigma_t[i] <- tos_sigma^2
+      garch_loglik[i] <- log(dfas2(data[i], delta=tos_delta,mu=tos_mu,sigma=sigma_t[i] %>% sqrt(),lambda=tos_lambda))
     }
     for(i in (p+1):t){
       sigma_t[i] <- exp(params[1]) +
         exp(params[2:(p+1)]) * sigma_t[(i-p):(i-1)] +
         exp(params[(p+2):(p+q+1)]) * data[(i-q):(i-1)]^2
-      garch_loglik[i] <- log(dfas2(data[i], mean = 0, sd = sqrt(sigma_t[i])))
+      garch_loglik[i] <- log(dfas2(data[i], delta=tos_delta,mu=tos_mu,sigma=sigma_t[i] %>% sqrt(),lambda=tos_lambda))
     }
     return(-sum(garch_loglik))
     }
   
   if(grepl(error,"norm")){out <- optim(params, garch_op)}else{
-    if(grepl(error,"ToS")){out <- optim(params, garch_op_ToS)}
+    if(grepl(error,"ToS")){
+      tos_mu <- dis_para["mu"]
+      tos_sigma <- dis_para["sigma"]
+      tos_delta <- dis_para["delta"]
+      tos_lambda <- dis_para["lambda"]
+      out <- optim(params, garch_op_ToS)}
   }
   out
 }
 
+
 # garchモデルのsigmaを計算する関数
-garch_f <- function(data, params, order = c(1, 1), error = "norm"){
+garch_f <- function(data, params, order = c(1, 1), error = "norm", dis_para){
   t <- length(data)
   sigma_t <- rep(0, t)
   if(length(params) != order[1] + order[2] + 1 ){
@@ -55,13 +63,22 @@ garch_f <- function(data, params, order = c(1, 1), error = "norm"){
   q <- order[2]
   #sigma_tの計算
   #計算できないところまでは、全体のボラティリティを与える
-  for(i in 1:p){
-    sigma_t[i] <- sum(data^2)/t
+  if(grepl(error,"norm")){
+    for(i in 1:p){
+      sigma_t[i] <- sum(data^2)/t
+    }
+  }else{
+    if(grepl(error,"ToS")){
+      for(i in 1:p){
+        sigma_t[i] <- dis_para["sigma"]^2
+      }
+    }
   }
   for(i in (p+1):t){
     sigma_t[i] <- exp(params[1]) +
       exp(params[2:(p+1)]) * sigma_t[(i-p):(i-1)] +
       exp(params[(p+2):(p+q+1)]) * data[(i-q):(i-1)]^2
   }
+  
   return(sigma_t)
 }
