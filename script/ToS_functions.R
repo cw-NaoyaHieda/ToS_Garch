ee99ea55d13dddfceba1997f37dda19c36b283a1
da-hie <hero12185@gmail.com>
2018-06-29 20:05:36 +0900

    GARCHpreまで
    GARCHで予測するところまで

--- script/ToS_functions.R
+++ script/ToS_functions.R
@@ -59,7 +59,7 @@ fas2.moment<- function(par){
   b2 <- integrate( f4, lower=-Inf, upper=Inf)$value
   return(list(m1=m1, v2=v2,b1=b1, b2=b2)) 
 }
-# 局度変換を伴うsinh-arcsinh(x)分布の平均，分散，歪度，尖度を計算する関数
+# 局度変換を伴うsinh-arcsinh(x)分布の分散を計算する関数
 fas2.var<- function(par){
   f1 <- function(x) x*dfas2(x, mu=par[1], sigma=par[2], lambda=par[3], delta=par[4])
   m1 <- integrate( f1, lower=-Inf, upper=Inf)$value
@@ -95,3 +95,242 @@ Resample1 <- function(data, weight, NofSample){
   newdata <- data[ (st+1) ]
   return(newdata)
 }
+
+
+#Fsaからの乱数 SIR並列処理
+rfa_SIR_para <- function(n, mu, sigma, lambda, delta)
+{
+  ## 正規分布を提案分布に
+  q <- as.vector(parSapply(cl,1:cl_l,
+                           function(x) rnorm(n/cl_l,mean=mu,sd=5*sigma)))
+  ## 重み
+  w <-parSapply(cl, q, 
+                dfas2, mu=mu, sigma=sigma, lambda=lambda, delta=delta)/
+    parSapply(cl, q, dnorm, mean=mu, sd=5*sigma)
+  ## 合計が1になるように重みを基準化
+  w <- w/sum(w)
+  ## 重みに従ってresample
+  q.resample <- Resample_para(q, weight=w, NofSample = n)
+  list(q,q=q.resample, w=w)
+}
+
+## 重点サンンプリングに用いるResampleの関数
+Resample_para <- function(data, weight, NofSample){
+  re_ind <- as.vector(parSapply(cl, 1:cl_l, function(x)runif(NofSample/cl_l) ))
+  cmwt <- cumsum(weight)/sum(weight);
+  st <- parSapply(cl,re_ind, function(x) sum(x>cmwt[-length(cmwt)]))
+  newdata <- data[ (st+1) ]
+  return(newdata)
+}
+
+#局度変換を伴うsinh-arcsinh分布の単純モンテカルロ法によってESを求める関数
+SMC.fa_para <-function(theta){
+  out <- c()
+  for(i in 1:100){
+    ## 正規分布からの重点サンプリングで乱数を取得
+    rand.fa<-rfa_SIR_para(n=20000, mu=theta[1], 
+                          sigma=theta[2], 
+                          lambda=theta[3],
+                          delta=theta[4])
+    y <- sample(rand.fa$q,10000)
+    
+    # 単純モンテカルロ法
+    #VaRの計算
+    VaR1 <- parSapply(cl,100:10000, function(x) quantile(y[1:x], c(0.01,0.025, 0.05)))
+    # ESの計算
+    Es1 <- parSapply(cl,100:10000, function(x) {
+      return(c( mean(y[1:x][y[1:x] < VaR1[1,x-99]]),   
+                mean(y[1:x][y[1:x] < VaR1[2,x-99]]),
+                mean(y[1:x][y[1:x] < VaR1[3,x-99]])
+      ))})
+    
+    
+    
+    # 真値と単純モンテカルロ法の結果をまとめる
+    out <- cbind(out,t(VaR1),t(Es1))
+  }
+  return(out)
+} 
+
+
+# 重点サンプリングの関数(d.ISを提案分布に局度変換を伴うsinh-arcsinh分布のESを求める)
+IS.fa_single <- function(){
+  f <- function(x, theta, par){
+    exp(theta*x)*dfas2(x, mu=par[1], sigma=par[2],
+                       lambda=par[3], delta=par[4])
+  }
+  #weightを計算するためにMを計算する．99%,97.5%,95%をまとめて行う
+  M1 <- integrate(f, -30, 30, theta=theta.val1, par=fit$par2)$value
+  M25 <- integrate(f, -30, 30, theta=theta.val25, par=fit$par2)$value
+  M5 <- integrate(f, -30, 30, theta=theta.val5, par=fit$par2)$value
+  
+  #weightを計算する
+  w1 <- exp(-theta.val1*rfa1)*M1
+  w25 <- exp(-theta.val25*rfa25)*M25
+  w5 <- exp(-theta.val5*rfa5)*M5
+  
+  N <-10000
+  
+  #99%点での計算 100~10000までサンプル数を増やして行う
+  out1<- sapply(100:N, function(x){
+    # サンプルの対応するweightをくっつける
+    out1<-cbind( rfa1[1:x],  w1[1:x]/x)
+    # サンプルの小さい順にならべかえる
+    A <- out1[sort.list(out1[,1]),]
+    # weightの累積和を並べる
+    A <- cbind(A, cumsum(A[,2]))
+    # 累積和が0.01に一番近いサンプルが99%VaR
+    # v1までのサンプルからES0.01の推定値を求める
+    v1 <- A[which.min(abs(A[,3]-0.01)),1]
+    es1<- sum(apply(A[1:which.min(abs(A[,3]-0.01)),1:2],1,prod))/0.01
+    return(c(v1,es1))})
+  
+  #matplot(t(out1),type="l")
+  #abline(h=VaR.true.FA[1],col=1)
+  #abline(h=ES.true.FA[1],col=2)
+  
+  out25<-sapply(100:N, function(x){
+    out1<-cbind(rfa25[1:x],  w25[1:x]/x)
+    A <- out1[sort.list(out1[,1]),]
+    A <- cbind(A, cumsum(A[,2]))
+    v1 <- A[which.min(abs(A[,3]-0.025)),1]
+    es1<- sum(apply(A[1:which.min(abs(A[,3]-0.025)),1:2],1,prod))/0.025
+    return(c(v1,es1))})
+  
+  #matplot(t(out25),type="l")
+  #abline(h=VaR.true.FA[2],col=1)
+  #abline(h=ES.true.FA[2],col=2)
+  
+  out5<-sapply(100:N, function(x){
+    out1<-cbind( rfa5[1:x],  w5[1:x]/x)
+    A <- out1[sort.list(out1[,1]),]
+    A <- cbind(A, cumsum(A[,2]))
+    v1 <- A[which.min(abs(A[,3]-0.05)),1]
+    es1<- sum(apply(A[1:which.min(abs(A[,3]-0.05)),1:2],1,prod))/0.05
+    return(c(v1,es1))})
+  
+  #matplot(t(out5),type="l")
+  #abline(h=VaR.true.FA[3],col=1)
+  #abline(h=ES.true.FA[3],col=2)
+  
+  
+  return(out = cbind(t(out1),t(out25),t(out5)))
+  
+}
+
+find.theta <- function(p, par){
+  VaR<- qfas(p, mu=par[1], sigma=par[2], lambda=par[3], delta=par[4])
+  f <- function(x) return( mean.IS(x,  par) -VaR )
+  out <- uniroot(f, interval=c(-10,10),extendInt="yes", trace=1)$root
+  out}
+
+# 重点分布の平均を計算する関数
+mean.IS <- function(theta,par){
+  f <- function(x, theta, par){
+    exp(theta*x)*dfas2(x, mu=par[1], sigma=par[2],
+                       lambda=par[3], delta=par[4])
+  }
+  #M <- integrate(f, -Inf, Inf, theta=theta, par=par)$value
+  M <- integrate(f, -30, 30, theta=theta, par=par)$value
+  m.f<- function(x, theta, par)  x*f(x,theta,par)/M
+  # return( integrate(m.f, -Inf, Inf, theta=theta, par=par)$value )
+  return( integrate(m.f, -30, 30, theta=theta, par=par)$value )
+}
+
+### 指数変換した重点分布の密度関数  
+d.IS <- function(x, theta,par){
+  # 重点分布の密度関数の分子
+  f <- function(x, theta, par){
+    exp(theta*x)*dfas2(x, mu=par[1], sigma=par[2],
+                       lambda=par[3], delta=par[4])
+  }
+  # 分母
+  M <- integrate(f, -30, 30, theta=theta, par=par)$value
+  return( f(x, theta, par)/M )
+}
+
+
+find.theta <- function(p, par){
+  VaR<- qfas(p, mu=par[1], sigma=par[2], lambda=par[3], delta=par[4])
+  f <- function(x) return( mean.IS(x,  par) -VaR )
+  out <- uniroot(f, interval=c(-10,10),extendInt="yes", trace=1)$root
+  out}
+
+
+### 重点分布からサンプリングをする 
+### 指数変換した重点分布の密度関数  
+d.IS <- function(x, theta,par){
+  # 重点分布の密度関数の分子
+  f <- function(x, theta, par){
+    exp(theta*x)*dfas2(x, mu=par[1], sigma=par[2],
+                       lambda=par[3], delta=par[4])
+  }
+  # 分母
+  M <- integrate(f, -30, 30, theta=theta, par=par)$value
+  return( f(x, theta, par)/M )
+}
+
+
+
+# 重点サンプリングの関数(d.ISを提案分布に局度変換を伴うsinh-arcsinh分布のVaR,ESを求める)
+IS.fa <- function(){
+  f <- function(x, theta, par){
+    exp(theta*x)*dfas2(x, mu=par[1], sigma=par[2],
+                       lambda=par[3], delta=par[4])
+  }
+  #weightを計算するためにMを計算する．99%,97.5%,95%をまとめて行う
+  M1 <- integrate(f, -30, 30, theta=theta.val1, par=fit$par2)$value
+  M25 <- integrate(f, -30, 30, theta=theta.val25, par=fit$par2)$value
+  M5 <- integrate(f, -30, 30, theta=theta.val5, par=fit$par2)$value
+  
+  #weightを計算する
+  w1 <- exp(-theta.val1*rfa1)*M1
+  w25 <- exp(-theta.val25*rfa25)*M25
+  w5 <- exp(-theta.val5*rfa5)*M5
+  
+  N <-10000
+  
+  #99%点での計算 100~10000までサンプル数を増やして行う
+  out1<- sapply( 100:N, function(x){
+    # サンプルの対応するweightをくっつける
+    out1<-cbind( rfa1[1:x],  w1[1:x]/x)
+    # サンプルの小さい順にならべかえる
+    A <- out1[sort.list(out1[,1]),]
+    # weightの累積和を並べる
+    A <- cbind(A, cumsum(A[,2]))
+    # 累積和が0.01に一番近いサンプルが99%VaR
+    v1 <- A[which.min(abs(A[,3]-0.01)),1]
+    # v1までのサンプルからES0.01の推定値を求める
+    es1 <- sum(apply(A[1:which.min(abs(A[,3]-0.01)),1:2],1,prod))/0.01
+    return(c(v1, es1))})
+  
+  #matplot(t(out1),type="l")
+  #abline(h=VaR.true.FA[1],col=1)
+  #abline(h=ES.true.FA[1],col=2)
+  
+  out25<-sapply(100:N, function(x){
+    out1<-cbind( rfa25[1:x],  w25[1:x]/x)
+    A <- out1[sort.list(out1[,1]),]
+    A <- cbind(A, cumsum(A[,2]))
+    v1<-A[which.min(abs(A[,3]-0.025)),1]
+    es1<-   sum(apply(A[1:which.min(abs(A[,3]-0.025)),1:2],1,prod))/0.025
+    return(c(v1, es1))})
+  
+  #matplot(t(out25),type="l")
+  #abline(h=VaR.true.FA[2],col=1)
+  #abline(h=ES.true.FA[2],col=2)
+  
+  out5<-sapply(100:N, function(x){
+    out1<-cbind( rfa5[1:x],  w5[1:x]/x)
+    A <- out1[sort.list(out1[,1]),]
+    A <- cbind(A, cumsum(A[,2]))
+    v1<-A[which.min(abs(A[,3]-0.05)),1]
+    es1<- sum(apply(A[1:which.min(abs(A[,3]-0.05)),1:2],1,prod))/0.05
+    return(c(v1, es1))})
+  
+  #matplot(t(out5),type="l")
+  #abline(h=VaR.true.FA[3],col=1)
+  #abline(h=ES.true.FA[3],col=2)
+  
+  
+  return( list(out = cbind(t(out1),t(out25),t(out5)), VaR.true.FA, ES.true.FA ) ) }
--- script/functions.R
+++ script/functions.R
@@ -1,55 +1,144 @@
+# GARCHを適用する関数
 garch_f_opt <- function(data, order = c(1, 1), error = "norm", dis_para = c()){
   t <- length(data)
   sigma_t <- rep(0, t)
   garch_loglik <- rep(0, t)
-  p <- order[1]
-  q <- order[2]
-  params <- rep(0,c(p + q + 1))
-  # garchモデルの対数尤度を計算する関数
+  #GARCHのbeta
+  G_beta <- order[1]
+  #GARCHのalpha
+  G_alpha <- order[2]
+  params <- rep(0,c(G_beta + G_alpha + 1))
+  # garchモデルの対数尤度を計算する関数(関数によって変えてますが、多分もっとスマートに書けますね・・・)
   garch_op <- function(params){
     #sigma_tの計算
     #計算できないところまでは、全体のボラティリティを与える
-    for(i in 1:p){
+    for(i in 1:G_beta){
       sigma_t[i] <- sum(data^2)/t
       garch_loglik[i] <- log(dnorm(data[i], mean = 0, sd = sqrt(sigma_t[i])))
     }
-    for(i in (p+1):t){
+    for(i in (G_beta+1):t){
       sigma_t[i] <- exp(params[1]) +
-        exp(params[2:(p+1)]) * sigma_t[(i-p):(i-1)] +
-        exp(params[(p+2):(p+q+1)]) * data[(i-q):(i-1)]^2
+        exp(params[2:(G_beta+1)]) * sigma_t[(i-G_beta):(i-1)] +
+        exp(params[(G_beta+2):(G_beta+G_alpha+1)]) * data[(i-G_alpha):(i-1)]^2
       garch_loglik[i] <- log(dnorm(data[i], mean = 0, sd = sqrt(sigma_t[i])))
     }
     return(-sum(garch_loglik))
-    }
-    
+  }
+  
+  # garchモデルの対数尤度を計算する関数(FA分布版)  
   garch_op_ToS <- function(params){
     #sigma_tの計算
-    #計算できないところまでは、全体のボラティリティを与える
-    for(i in 1:p){
+    #計算できないところまでは、仮に推定してるsigmaを与える
+    for(i in 1:G_beta){
       #sigma_t[i] <- sum(data^2)/t
       # ToS のsigma!=標準偏差なので
       sigma_t[i] <- tos_sigma^2
       garch_loglik[i] <- log(dfas2(data[i], delta=tos_delta,mu=tos_mu,sigma=sigma_t[i] %>% sqrt(),lambda=tos_lambda))
     }
-    for(i in (p+1):t){
+    for(i in (G_beta+1):t){
       sigma_t[i] <- exp(params[1]) +
-        exp(params[2:(p+1)]) * sigma_t[(i-p):(i-1)] +
-        exp(params[(p+2):(p+q+1)]) * data[(i-q):(i-1)]^2
+        exp(params[2:(G_beta+1)]) * sigma_t[(i-G_beta):(i-1)] +
+        exp(params[(G_beta+2):(G_beta+G_alpha+1)]) * data[(i-G_alpha):(i-1)]^2
       garch_loglik[i] <- log(dfas2(data[i], delta=tos_delta,mu=tos_mu,sigma=sigma_t[i] %>% sqrt(),lambda=tos_lambda))
     }
     return(-sum(garch_loglik))
+  }
+  
+  # garchモデルの対数尤度を計算する関数(FA分布版)  
+  garch_op_ToS_stand <- function(params){
+    #sigma_tの計算
+    #計算できないところまでは、仮に推定してるsigmaを与える
+    for(i in 1:G_beta){
+      #sigma_t[i] <- sum(data^2)/t
+      # ToS のsigma!=標準偏差なので
+      sigma_t[i] <- sum(data^2)/t
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta,mu=tos_mu,sigma=tos_sigma,lambda=tos_lambda,sd=sigma_t[i]%>% sqrt()))
     }
+    for(i in (G_beta+1):t){
+      sigma_t[i] <- exp(params[1]) +
+        exp(params[2:(G_beta+1)]) * sigma_t[(i-G_beta):(i-1)] +
+        exp(params[(G_beta+2):(G_beta+G_alpha+1)]) * data[(i-G_alpha):(i-1)]^2
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta,mu=tos_mu,sigma=tos_sigma,lambda=tos_lambda,sd=sigma_t[i]%>% sqrt()))
+    }
+    return(-sum(garch_loglik))
+  }
+  
+  # garchモデルの対数尤度を計算する関数(FA分布版)  
+  garch_op_ToS_stand_with_roll <- function(params){
+    #sigma_tの計算
+    #計算できないところまでは、仮に推定してるsigmaを与える
+    for(i in 1:G_beta){
+      #sigma_t[i] <- sum(data^2)/t
+      # ToS のsigma!=標準偏差なので
+      sigma_t[i] <- sum(data^2)/t
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta[i],mu=tos_mu[i],
+                                            sigma=tos_sigma[i],lambda=tos_lambda[i],sd=sigma_t[i]%>% sqrt()))
+    }
+    for(i in (G_beta+1):t){
+      sigma_t[i] <- exp(params[1]) +
+        exp(params[2:(G_beta+1)]) * sigma_t[(i-G_beta):(i-1)] +
+        exp(params[(G_beta+2):(G_beta+G_alpha+1)]) * data[(i-G_alpha):(i-1)]^2
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta[i],mu=tos_mu[i],
+                                            sigma=tos_sigma[i],lambda=tos_lambda[i],sd=sigma_t[i]%>% sqrt()))
+    }
+    return(-sum(garch_loglik))
+  }
+  
+  # garchモデルの対数尤度を計算する関数(FA分布版)  
+  garch_op_ToS_with_roll <- function(params){
+    #sigma_tの計算
+    #計算できないところまでは、仮に推定してるsigmaを与える
+    for(i in 1:G_beta){
+      #sigma_t[i] <- sum(data^2)/t
+      # ToS のsigma!=標準偏差なので
+      sigma_t[i] <- sum(data^2)/t
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta[i],mu=tos_mu[i],
+                                   sigma=sigma_t[i] ,lambda=tos_lambda[i],sd=sigma_t[i]%>% sqrt()))
+    }
+    for(i in (G_beta+1):t){
+      sigma_t[i] <- exp(params[1]) +
+        exp(params[2:(G_beta+1)]) * sigma_t[(i-G_beta):(i-1)] +
+        exp(params[(G_beta+2):(G_beta+G_alpha+1)]) * data[(i-G_alpha):(i-1)]^2
+      garch_loglik[i] <- log(dfas2(data[i], delta=tos_delta[i],mu=tos_mu[i],
+                                   sigma=sigma_t[i] ,lambda=tos_lambda[i]))
+    }
+    return(-sum(garch_loglik))
+  }
   
   if(grepl(error,"norm")){out <- optim(params, garch_op)}else{
-    if(grepl(error,"ToS")){
+    if(grepl(error,"ToS") && is.null(dim(dis_para))){
       tos_mu <- dis_para["mu"]
       tos_sigma <- dis_para["sigma"]
       tos_delta <- dis_para["delta"]
       tos_lambda <- dis_para["lambda"]
-      out <- optim(params, garch_op_ToS)}
+      out <- optim(params, garch_op_ToS)}else{
+        if(grepl(error,"ToS_stand")&& is.null(dim(dis_para))){
+          tos_mu <- dis_para["mu"]
+          tos_sigma <- dis_para["sigma"]
+          tos_delta <- dis_para["delta"]
+          tos_lambda <- dis_para["lambda"]
+          out <- optim(params, garch_op_ToS_stand)
+      }else{
+        if(grepl(error,"ToS_stand")){
+          tos_mu <- pull(dis_para,mu)
+          tos_sigma <- pull(dis_para,sigma)
+          tos_delta <- pull(dis_para,delta)
+          tos_lambda <- pull(dis_para,lambda)
+          out <- optim(params, garch_op_ToS_stand_with_roll)
+        }else{
+        if(grepl(error,"ToS")){
+          tos_mu <- pull(dis_para,mu)
+          tos_sigma <- pull(dis_para,sigma)
+          tos_delta <- pull(dis_para,delta)
+          tos_lambda <- pull(dis_para,lambda)
+          out <- optim(params, garch_op_ToS_with_roll)
+        }
+        }
+      }
+      }
   }
   out
-}
+  }
 
 
 # garchモデルのsigmaを計算する関数
@@ -68,10 +157,22 @@ garch_f <- function(data, params, order = c(1, 1), error = "norm", dis_para){
       sigma_t[i] <- sum(data^2)/t
     }
   }else{
-    if(grepl(error,"ToS")){
+    if(grepl(error,"ToS") && is.null(dim(dis_para))){
       for(i in 1:p){
         sigma_t[i] <- dis_para["sigma"]^2
       }
+    }else{
+      if(grepl(error,"ToS_stand")){
+        for(i in 1:p){
+          sigma_t[i] <- sum(data^2)/t
+        }
+      }else{
+      if(grepl(error,"ToS")){
+        for(i in 1:p){
+          sigma_t[i] <- pull(dis_para,sigma)[i]^2
+        }
+    }
+    }
     }
   }
   for(i in (p+1):t){
@@ -81,4 +182,199 @@ garch_f <- function(data, params, order = c(1, 1), error = "norm", dis_para){
   }
   
   return(sigma_t)
+}
+
+
+
+# 流石にややこしいので、ToS分布のパラメータまで推定する分布は別版
+garch_f_opt_with_para <- function(data, order = c(1, 1)){
+  t <- length(data)
+  sigma_t <- rep(0, t)
+  garch_loglik <- rep(0, t)
+  #GARCHのbeta
+  G_beta <- order[1]
+  #GARCHのalpha
+  G_alpha <- order[2]
+  params <- rep(0,c(G_beta + G_alpha + 1 + 4))
+  params_len <- length(params)
+  params[(params_len-3):params_len] <- c(0, log(0.2), -0.2, 0.5)
+  
+  # garchモデルの対数尤度を計算する関数(FA分布版)  
+  garch_op_ToS_stand <- function(params){
+    tos_mu <- params[params_len - 3]
+    tos_sigma <- exp(params[params_len - 2])
+    tos_lambda <- params[params_len - 1]
+    tos_delta <- params[params_len]
+    #sigma_tの計算
+    #計算できないところまでは、仮に推定してるsigmaを与える
+    for(i in 1:G_beta){
+      #sigma_t[i] <- sum(data^2)/t
+      # ToS のsigma!=標準偏差なので
+      sigma_t[i] <- sum(data^2)/t
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta,mu=tos_mu,sigma=tos_sigma,lambda=tos_lambda,sd=sigma_t[i]%>% sqrt()))
+    }
+    for(i in (G_beta+1):t){
+      sigma_t[i] <- exp(params[1]) +
+        exp(params[2:(G_beta+1)]) * sigma_t[(i-G_beta):(i-1)] +
+        exp(params[(G_beta+2):(G_beta+G_alpha+1)]) * data[(i-G_alpha):(i-1)]^2
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta,mu=tos_mu,sigma=tos_sigma,lambda=tos_lambda,sd=sigma_t[i]%>% sqrt()))
+    }
+    return(-sum(garch_loglik))
+  }
+ 
+  out <- optim(params, garch_op_ToS_stand)
+     
+  out
+}
+
+
+# 流石にややこしいので、ToS分布のパラメータまで推定する分布は別版
+garch_f_opt_with_para2 <- function(data, order = c(1, 1), tos_mu, tos_sigma){
+  t <- length(data)
+  sigma_t <- rep(0, t)
+  garch_loglik <- rep(0, t)
+  #GARCHのbeta
+  G_beta <- order[1]
+  #GARCHのalpha
+  G_alpha <- order[2]
+  params <- rep(0,c(G_beta + G_alpha + 1 + 2))
+  params_len <- length(params)
+  params[c(params_len-1,params_len)] <- c(-0.2, 0.5)
+  
+  # garchモデルの対数尤度を計算する関数(FA分布版)  
+  garch_op_ToS_stand <- function(params){
+    tos_lambda <- params[params_len - 1]
+    tos_delta <- params[params_len]
+    #sigma_tの計算
+    #計算できないところまでは、仮に推定してるsigmaを与える
+    for(i in 1:G_beta){
+      #sigma_t[i] <- sum(data^2)/t
+      # ToS のsigma!=標準偏差なので
+      sigma_t[i] <- sum(data^2)/t
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta,mu=tos_mu,sigma=tos_sigma,lambda=tos_lambda,sd=sigma_t[i]%>% sqrt()))
+    }
+    for(i in (G_beta+1):t){
+      sigma_t[i] <- exp(params[1]) +
+        exp(params[2:(G_beta+1)]) * sigma_t[(i-G_beta):(i-1)] +
+        exp(params[(G_beta+2):(G_beta+G_alpha+1)]) * data[(i-G_alpha):(i-1)]^2
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta,mu=tos_mu,sigma=tos_sigma,lambda=tos_lambda,sd=sigma_t[i]%>% sqrt()))
+    }
+    return(-sum(garch_loglik))
+  }
+  
+  out <- optim(params, garch_op_ToS_stand)
+  
+  out
+}
+
+# lambdaは0に固定してやる
+garch_f_opt_with_para2_lambda0 <- function(data, order = c(1, 1), tos_mu, tos_sigma){
+  t <- length(data)
+  sigma_t <- rep(0, t)
+  garch_loglik <- rep(0, t)
+  #GARCHのbeta
+  G_beta <- order[1]
+  #GARCHのalpha
+  G_alpha <- order[2]
+  params <- rep(0,c(G_beta + G_alpha + 1 + 1))
+  params_len <- length(params)
+  params[c(params_len)] <- c(0.5)
+  tos_lambda <- 0
+  # garchモデルの対数尤度を計算する関数(FA分布版)  
+  garch_op_ToS_stand <- function(params){
+    tos_delta <- params[params_len]
+    #sigma_tの計算
+    #計算できないところまでは、仮に推定してるsigmaを与える
+    for(i in 1:G_beta){
+      #sigma_t[i] <- sum(data^2)/t
+      # ToS のsigma!=標準偏差なので
+      sigma_t[i] <- sum(data^2)/t
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta,mu=tos_mu,sigma=tos_sigma,lambda=tos_lambda,sd=sigma_t[i]%>% sqrt()))
+    }
+    for(i in (G_beta+1):t){
+      sigma_t[i] <- exp(params[1]) +
+        exp(params[2:(G_beta+1)]) * sigma_t[(i-G_beta):(i-1)] +
+        exp(params[(G_beta+2):(G_beta+G_alpha+1)]) * data[(i-G_alpha):(i-1)]^2
+      garch_loglik[i] <- log(dfas2_stand_sd(data[i], delta=tos_delta,mu=tos_mu,sigma=tos_sigma,lambda=tos_lambda,sd=sigma_t[i]%>% sqrt()))
+    }
+    return(-sum(garch_loglik))
+  }
+  
+  out <- optim(params, garch_op_ToS_stand)
+  
+  out
+}
+
+# mu+sigma*errorで行う
+garch_f_opt_with_para2_muestimate <- function(data, order = c(1, 1), tos_mu, tos_sigma){
+  t <- length(data)
+  sigma_t <- rep(0, t)
+  garch_loglik <- rep(0, t)
+  #GARCHのbeta
+  G_beta <- order[1]
+  #GARCHのalpha
+  G_alpha <- order[2]
+  params <- rep(0,c(G_beta + G_alpha + 1 +1 + 2))
+  params_len <- length(params)
+  params[c(params_len-1,params_len)] <- c(-0.2, 0.5)
+  # garchモデルの対数尤度を計算する関数(FA分布版)  
+  garch_op_ToS_stand <- function(params){
+    tos_lambda <- params[params_len - 1]
+    tos_delta <- params[params_len]
+    #sigma_tの計算
+    #計算できないところまでは、仮に推定してるsigmaを与える
+    for(i in 1:G_beta){
+      #sigma_t[i] <- sum(data^2)/t
+      # ToS のsigma!=標準偏差なので
+      sigma_t[i] <- sum(data^2)/t
+      garch_loglik[i] <- log(dfas2_stand_sd((data[i] - params[params_len - 2]), delta=tos_delta,mu=tos_mu,sigma=tos_sigma,
+                                            lambda=tos_lambda,sd=sigma_t[i]%>% sqrt()))
+    }
+    for(i in (G_beta+1):t){
+      sigma_t[i] <- exp(params[1]) +
+        exp(params[2:(G_beta+1)]) * sigma_t[(i-G_beta):(i-1)] +
+        exp(params[(G_beta+2):(G_beta+G_alpha+1)]) * data[(i-G_alpha):(i-1)]^2
+      garch_loglik[i] <- log(dfas2_stand_sd( (data[i] - params[params_len - 2]), delta=tos_delta,mu=tos_mu,sigma=tos_sigma,
+                                            lambda=tos_lambda,sd=sigma_t[i]%>% sqrt()))
+    }
+    return(-sum(garch_loglik))
+  }
+  
+  out <- optim(params, garch_op_ToS_stand)
+  
+  out
+}
+
+
+# 正規分布で平均を考慮する
+garch_f_opt_with_para_norm <- function(data, order = c(1, 1), tos_mu, tos_sigma){
+  t <- length(data)
+  sigma_t <- rep(0, t)
+  garch_loglik <- rep(0, t)
+  #GARCHのbeta
+  G_beta <- order[1]
+  #GARCHのalpha
+  G_alpha <- order[2]
+  params <- rep(0,c(G_beta + G_alpha + 1 + 1))
+  params_len <- length(params)
+  # garchモデルの対数尤度を計算する関数(FA分布版)  
+  garch_op <- function(params){
+    #sigma_tの計算
+    #計算できないところまでは、全体のボラティリティを与える
+    for(i in 1:G_beta){
+      sigma_t[i] <- sum(data^2)/t
+      garch_loglik[i] <- log(dnorm(data[i], mean = params[params_len], sd = sqrt(sigma_t[i])))
+    }
+    for(i in (G_beta+1):t){
+      sigma_t[i] <- exp(params[1]) +
+        exp(params[2:(G_beta+1)]) * sigma_t[(i-G_beta):(i-1)] +
+        exp(params[(G_beta+2):(G_beta+G_alpha+1)]) * data[(i-G_alpha):(i-1)]^2
+      garch_loglik[i] <- log(dnorm(data[i], mean = params[params_len], sd = sqrt(sigma_t[i])))
+    }
+    return(-sum(garch_loglik))
+  }
+  
+  out <- optim(params, garch_op)
+  
+  out
 }
